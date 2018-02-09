8b0aadb43e3c96076dcfc4980ece555a7a6b8090
Long Cheng <kevinclcn@gmail.com>
2018-02-09 10:24:25 +0800

    add unit test to assure Log4e and Entry4e to qualify echo.Logger

--- log4e_test.go
+++ log4e_test.go
@@ -4,14 +4,16 @@ import (
 	"testing"
 	"bytes"
 	"github.com/stretchr/testify/assert"
+	"github.com/labstack/echo"
+	"github.com/labstack/gommon/log"
 )
 
 func TestLogger(t *testing.T)  {
 	buf := bytes.NewBuffer([]byte{})
 	logger.Out = buf
 	Info("hello")
-	assert.Contains(t, buf.String(), "msg=hello", "log should contain hello")
-	assert.Contains(t, buf.String(), "level=info", "log should be info level")
+	assert.Contains(t, buf.String(), "msg=hello", "log")
+	assert.Contains(t, buf.String(), "level=info", "log")
 }
 
 func TestWithFields(t *testing.T) {
@@ -23,4 +25,20 @@ func TestWithFields(t *testing.T) {
 	}).Info()
 
 	assert.Contains(t, buf.String(), "requestId=123", "")
+}
+
+func TestEchoLogger(t *testing.T)  {
+	var v echo.Logger
+	v = logger
+	buf := bytes.NewBuffer([]byte{})
+	v.SetOutput(buf)
+
+	json := log.JSON{"msg": "I'm a test message"}
+	v.Infoj(json)
+
+	assert.Contains(t, buf.String(), "msg=\"I'm a test message\"", "log")
+
+	v = WithFields(Fields{"field1":"field1"})
+	v.Infoj(json)
+	assert.Contains(t, buf.String(), "field1=field1", "")
 }
